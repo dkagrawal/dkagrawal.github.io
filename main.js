d900edfcae11890e870269f52001388141809782
dkagrawal <dishank.agrawal123@gmail.com>
2015-03-26 18:51:59 +0530

    Add javascript to my page.

---
+++ main.js
@@ -0,0 +1,18 @@
+var red = [0, 100, 63];
+var orange = [40, 100, 60];
+var green = [75, 100, 40];
+var blue = [196, 77, 55];
+var purple = [280, 50, 60];
+var tardis = [230, 58, 24];
+var violet = [76, 28, 49];
+var myName = "Dishank Agrawal";
+letterColors= [red,violet,orange,green,blue,purple,tardis];
+if(false){
+bubbleShape = "square";
+}
+else {
+bubbleShape = "circle";
+}
+
+drawName(myName, letterColors);
+bounceBubbles();
